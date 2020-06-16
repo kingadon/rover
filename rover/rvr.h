324acf6df6fc01ffcce333b4e01a20bb37086283
Kingston A Huynh <kingston.a.huynh@gmail.com>
2020-06-16 15:58:12 -0500

    add rvr_write_uint function with documentation

--- rover/rvr.h
+++ rover/rvr.h
@@ -16,4 +16,12 @@
  */
 extern int rvr_write(const char* path, char* data);
 
+/**
+ * @brief Write an unsigned integer as string into file specified by path.
+ * @param path Absolute path to the file.
+ * @param data Unsigned integer value to be written.
+ * @return Return the number of elements written or -1 if error.
+ */
+extern int rvr_write_uint(const char* path, size_t val);
+
 #endif

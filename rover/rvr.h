11450d8d136b1c81e146ccb3ca65b95d9a6978d6
Kingston A Huynh <kingston.a.huynh@gmail.com>
2020-06-16 14:38:32 -0500

    add rvr.h with doxygen header

---
+++ rover/rvr.h
@@ -0,0 +1,11 @@
+/**
+ * @file rvr.h
+ * @author Kingston A Huynh
+ * @date June 16 2020
+ * @brief File contains general utility functions and constants used thru out library.
+ */
+
+#ifndef RVR_H
+#define RVR_H
+
+#endif

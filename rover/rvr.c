d0445fc09496029c3713dd4f6885dc5305c67d20
Kingston A Huynh <kingston.a.huynh@gmail.com>
2020-07-20 06:24:18 -0500

    change file writes to use fprintf

--- rover/rvr.c
+++ rover/rvr.c
@@ -2,29 +2,56 @@
 #include <string.h>
 #include "rvr.h"
 
-const size_t MAX_BYTES = 8;
+const size_t MAX_BYTES = 32;
 const size_t MAX_PATH_BYTES = 64;
 const char* const ROOT_COMPONENT_DIR = "sys/class";
+const char* const PATH_FMT = "/%s/%s/%s";
 
-int rvrWrite(const char* path, char* data) {
+enum rvrValueType {
+    INTEGER,
+    STRING
+};
+
+extern struct rvrvalue {
+    enum rvrValueType type;
+    union {
+        int num;
+        char str[MAX_BYTES];
+    };
+};
+
+const char* const NUMBER_FORMAT = "%d\n";
+const char* const STRING_FORMAT = "%s\n";
+
+int rvrWrite(const char* path, const char* fmt, struct rvrvalue* val) {
     FILE* fptr = fopen(path, "w");
     if (!fptr) {
         perror("rvr_write:fopen");
         return -1;
     }
 
-    int elementsWritten = (int) fwrite(data, 1, strlen(data), fptr);
-    fclose(fptr);
-    return elementsWritten;
+    int charsWritten = -1;
+    if (val->type == INTEGER) {
+        charsWritten = fprintf(fptr, fmt, val->num);
+    } else {
+        charsWritten = fprintf(fptr, fmt, val->str);
+    }
+
+    return charsWritten;
+}
+
+int rvrWriteInt(const char* path, int val) {
+    char strVal[MAX_BYTES];
+    struct rvrvalue rvrval = {
+        .type = INTEGER,
+        .num = val
+    };
+
+    return rvrWrite(path, NUMBER_FORMAT, &rvrval); 
 }
 
 int rvrWriteUInt(const char* path, size_t val) {
     char strVal[MAX_BYTES];
-    int elementsWritten = sprintf(strVal, "%zu", val);
-    if (elementsWritten < 1) {
-        perror("rvr_write_uint:sprintf");
-        return -1;
-    }
-
-    return rvrWrite(path, strVal); 
+    return rvrWriteInt(path, val); 
 }
+
--- rover/rvr.h
+++ rover/rvr.h
@@ -18,20 +18,32 @@ extern const size_t MAX_PATH_BYTES;
 /** @brief Root directory of components. */
 extern const char* const ROOT_COMPONENT_DIR;
 
+/** @brief Format string for snprintf to build path. */
+extern const char* const PATH_FMT;
+
 /** End global constants. */
 
 /**
  * @brief Write a string into file specified by path.
  * @param path Absolute path to the file.
- * @param data String value to be written.
+ * @param fmt String value to be written.
+ * @param val String value to be written.
  * @return Return the number of elements written or -1 if error.
  */
-extern int rvrWrite(const char* path, char* data);
+extern int rvrWrite(const char* path, const char* fmt, struct rvrvalue* val);
+
+/**
+ * @brief Write an integer as string into file specified by path.
+ * @param path Absolute path to the file.
+ * @param val Integer value to be written.
+ * @return Return the number of elements written or -1 if error.
+ */
+extern int rvrWriteInt(const char* path, int val);
 
 /**
  * @brief Write an unsigned integer as string into file specified by path.
  * @param path Absolute path to the file.
- * @param data Unsigned integer value to be written.
+ * @param val Unsigned integer value to be written.
  * @return Return the number of elements written or -1 if error.
  */
 extern int rvrWriteUInt(const char* path, size_t val);

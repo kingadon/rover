4a297e3b35823b49af1ec99953ed758e3d71229a
Kingston A Huynh <kingston.a.huynh@gmail.com>
2020-07-23 07:13:37 -0500

    add more servo component paths

--- tests/rvrtest.cpp
+++ tests/rvrtest.cpp
@@ -15,9 +15,22 @@ char* getTestPath(enum ServoAttr attr) {
         case ServoAttr::DUTY_CYCLE_SP:
             return (char*) "tests/sys/class/tacho-motor/motor0/duty_cycle_sp"; 
             break;
-        default:
+        case ServoAttr::COMMAND:
             return (char*) "tests/sys/class/tacho-motor/motor0/command"; 
+        case ServoAttr::POSITION_SP:
+            return (char*) "tests/sys/class/tacho-motor/motor0/position_sp"; 
+        case ServoAttr::POLARITY:
+            return (char*) "tests/sys/class/tacho-motor/motor0/polarity"; 
+        case ServoAttr::RAMP_DOWN_SP:
+            return (char*) "tests/sys/class/tacho-motor/motor0/ramp_down_sp"; 
+        case ServoAttr::RAMP_UP_SP:
+            return (char*) "tests/sys/class/tacho-motor/motor0/ramp_up_sp"; 
+        case ServoAttr::STOP_ACTION:
+            return (char*) "tests/sys/class/tacho-motor/motor0/stop_action"; 
+        case ServoAttr::TIME_SP:
+            return (char*) "tests/sys/class/tacho-motor/motor0/time_sp"; 
     }
+    return (char*) ".";
 }
 
 void readTestStr(char* path, char* str) {
